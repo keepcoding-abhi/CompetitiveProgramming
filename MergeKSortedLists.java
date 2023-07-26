85395119431f989fa49ee5a0b387344b9c7f4a03
keepcoding-abhi <122338277+keepcoding-abhi@users.noreply.github.com>
2023-07-25 20:24:50 -0700

    Merge K sorted lists problem on Leetcode.

--- MergeKSortedLists.java
+++ MergeKSortedLists.java
@@ -1,7 +1,13 @@
+import java.util.PriorityQueue;
+
 public class MergeKSortedLists {
 }
 //1) Store all the elements in an array and sort it.
 // Then create a new list and store the values as per the order in sorted array
+/*
+Time : O((k * n) * Log(k * n)), here k is the number of lists and n is the average length in the lists.
+Space : O(k * n) for storing numbers in an array.
+ */
 
 class Solution {
     public ListNode mergeKLists(ListNode[] lists) {
@@ -39,6 +45,10 @@ class Solution {
 
 
 // 2) Compare One-by-One
+/*
+Time : O(n*k)
+Space ; O(1)
+ */
 
 /**
  * Definition for singly-linked list.
@@ -97,58 +107,49 @@ class Solution {
 }
 
 // 3) Compare with priority queue
+/*
+Time : O(N * Log(k))
+Space : O(k)
+ */
 class Solution {
     public ListNode mergeKLists(ListNode[] lists) {
-        ListNode resultHead = new ListNode();
-        ListNode resultTail = resultHead;
 
-        int numberOfLists = lists.length;
+        ListNode result = null;
 
-        if(numberOfLists > 0) {
-            PriorityQueue<Entry> nextElementsInLists = new PriorityQueue<Entry>(numberOfLists);
-            for(int index = 0 ; index < numberOfLists ; index++) {
-                ListNode currentList = lists[index];
-                if(currentList != null) {
-                    int currentValue = currentList.val;
+        if (lists != null && lists.length > 0) {
 
-                    Entry entry = new Entry(currentList);
-                    nextElementsInLists.add(entry);
+            PriorityQueue<ListNode> nodesToAdd = new PriorityQueue<ListNode>((ListNode a, ListNode b) -> {
+                return a.val - b.val;
+            });
+
+            for (int index = 0; index < lists.length; index++) {
+                ListNode currentNode = lists[index];
+
+                if (currentNode != null) {
+                    nodesToAdd.add(currentNode);
                 }
             }
 
-            while(!nextElementsInLists.isEmpty()) {
-                Entry nextElement = nextElementsInLists.remove();
-                ListNode listOfInterest = nextElement.getList();
+            if (!nodesToAdd.isEmpty()) {
+                result = nodesToAdd.poll();
+                ListNode currentNode = result;
 
-                resultTail.next = listOfInterest;
-                resultTail = resultTail.next;
+                if (currentNode.next != null) {
+                    nodesToAdd.add(currentNode.next);
+                }
 
-                listOfInterest = listOfInterest.next;
-                if(listOfInterest != null) {
-                    Entry nextEntry = new Entry(listOfInterest);
-                    nextElementsInLists.add(nextEntry);
+                while (!nodesToAdd.isEmpty()) {
+                    currentNode.next = nodesToAdd.poll();
+                    currentNode = currentNode.next;
+
+                    if (currentNode.next != null) {
+                        nodesToAdd.add(currentNode.next);
+                    }
                 }
             }
         }
 
-        return resultHead.next;
-    }
-}
-
-class Entry implements Comparable<Entry> {
-    private ListNode list;
-
-    public Entry(ListNode list) {
-        this.list = list;
-    }
-
-    public ListNode getList() {
-        return this.list;
-    }
-
-    @Override
-    public int compareTo (Entry other) {
-        return this.list.val - other.list.val;
+        return result;
     }
 }
 
@@ -250,6 +251,135 @@ class Solution {
         }
 
         return resultHead.next;
+    }
+}
+
+/*
+Time : O(N * k), N is the number of elements in the merged linked list.
+Space : O(k) for storing pointers
+select the least element from each of the k lists.
+ */
+class Solution {
+    public ListNode mergeKLists(ListNode[] lists) {
+
+        ListNode result = null;
+
+        if(lists != null && lists.length > 0) {
+            ListNode[] currentPointers = new ListNode[lists.length];
+            for(int index = 0 ; index < lists.length ; index++) {
+                currentPointers[index] = lists[index];
+            }
+
+            ListNode mergedListHead = null, mergedListCurr = null;
+
+            ListNode nextEl = getNextElement(currentPointers);
+
+            if(nextEl != null) {
+                mergedListHead = nextEl;
+                mergedListCurr = nextEl;
+
+                nextEl = getNextElement(currentPointers);
+
+                while(nextEl != null) {
+                    mergedListCurr.next = nextEl;
+                    mergedListCurr = mergedListCurr.next;
+                }
+            }
+
+            result = mergedListHead;
+        }
+
+        return result;
+
+    }
+
+    private ListNode getNextElement(ListNode[] lists) {
+        ListNode leastEl = null;
+
+        int index = 0, minIndex = -1;
+        for(ListNode currList : lists) {
+            if(currList != null) {
+                if(leastEl == null) {
+                    leastEl = currList;
+                    minIndex = index;
+                }
+                else if(currList.val < leastEl.val) {
+                    leastEl = currList;
+                    minIndex = index;
+                }
+            }
+
+            index++;
+        }
+
+        if(minIndex != -1) {
+            lists[minIndex] = lists[minIndex].next;
+        }
+
+        return leastEl;
+    }
+}
+
+/*
+Time : O(N * Log(k))
+Space : O(Log(k))
+The depth of recursion is Log(k) and at each level O(N) elements are merged.
+Divide and conquer solution
+ */
+class Solution {
+    public ListNode mergeKLists(ListNode[] lists) {
+
+        ListNode result = null;
+
+        if(lists != null && lists.length > 0) {
+
+            result = mergeByDAndC(lists, 0, lists.length - 1);
+        }
+
+        return result;
+    }
+
+    private ListNode mergeByDAndC(ListNode[] lists, int startIndex, int endIndex) {
+
+        ListNode mergedList = null;
+        if(startIndex == endIndex) {
+            mergedList = lists[startIndex];
+        }
+        else {
+            int midIndex = (startIndex + endIndex) / 2;
+            ListNode leftList = mergeByDAndC(lists, startIndex, midIndex);
+            ListNode rightList = mergeByDAndC(lists, midIndex + 1, endIndex);
+
+            mergedList = merge(leftList, rightList);
+        }
+
+        return mergedList;
+    }
+
+    private ListNode merge(ListNode list1, ListNode list2) {
+        ListNode mergedListHead = new ListNode();
+        ListNode currentNode = mergedListHead;
+
+        while(list1 != null && list2 != null) {
+            if(list1.val < list2.val) {
+                currentNode.next = list1;
+                list1 = list1.next;
+            }
+            else {
+                currentNode.next = list2;
+                list2 = list2.next;
+            }
+            currentNode = currentNode.next;
+        }
+
+        if(list1 != null) {
+            currentNode.next = list1;
+        }
+        else if(list2 != null) {
+            currentNode.next = list2;
+        }
+
+        return mergedListHead.next;
     }
 }
 
