90b502808ed06c7a3d5bce6d094f5772900cd6f5
keepcoding-abhi <122338277+keepcoding-abhi@users.noreply.github.com>
2023-08-11 12:57:58 -0700

    Same tree problem on Leetcode.

--- SameTree.java
+++ SameTree.java
@@ -1,22 +1,14 @@
+import java.util.Deque;
+import java.util.LinkedList;
+
 public class SameTree {
 }
 
-/**
- * Definition for a binary tree node.
- * public class TreeNode {
- *     int val;
- *     TreeNode left;
- *     TreeNode right;
- *     TreeNode() {}
- *     TreeNode(int val) { this.val = val; }
- *     TreeNode(int val, TreeNode left, TreeNode right) {
- *         this.val = val;
- *         this.left = left;
- *         this.right = right;
- *     }
- * }
- */
 class Solution {
+    /*
+    Time : O(n)
+    Space : O(n)
+     */
     public boolean isSameTree(TreeNode p, TreeNode q) {
 
         boolean sameTree = true;
@@ -41,5 +33,59 @@ class Solution {
         }
 
         return sameTree;
+    }
+
+    private boolean checkSameNodes(TreeNode p, TreeNode q) {
+        boolean same;
+
+        if(p == null && q == null) {
+            same = true;
+        }
+        else if(p != null && q != null) {
+            if(p.val == q.val /*&& ((p.left == null && q.left == null) || (p.left != null && q.left != null)) && ((p.right == null && q.right == null) || (p.right != null && q.right != null))*/) {
+                same = true;
+            }
+            else {
+                same = false;
+            }
+        }
+        else {
+            same = false;
+        }
+
+        return same;
+    }
+
+    /*
+    Iterative version of the above approach.
+    Time : O(n)
+    Space : O(n)
+     */
+    public boolean isSameTree(TreeNode p, TreeNode q) {
+        Deque<TreeNode> pNodesToProcess = new LinkedList<TreeNode>(), qNodesToProcess = new LinkedList<TreeNode>();
+
+        boolean same = true;
+
+        pNodesToProcess.push(p);
+        qNodesToProcess.push(q);
+
+        while(!pNodesToProcess.isEmpty() && !qNodesToProcess.isEmpty()) {
+            TreeNode pNode = pNodesToProcess.pop();
+            TreeNode qNode = qNodesToProcess.pop();
+            if(checkSameNodes(pNode, qNode)) {
+                if(pNode != null) {
+                    pNodesToProcess.push(pNode.left);
+                    pNodesToProcess.push(pNode.right);
+                    qNodesToProcess.push(qNode.left);
+                    qNodesToProcess.push(qNode.right);
+                }
+            }
+            else {
+                same = false;
+                break;
+            }
+        }
+
+        return same;
     }
 }
