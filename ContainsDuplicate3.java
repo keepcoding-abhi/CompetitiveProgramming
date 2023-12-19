215257c49261094945145b75ab7efeb5b59beb55
keepcoding-abhi <122338277+keepcoding-abhi@users.noreply.github.com>
2023-12-19 01:46:03 -0700

    Added bucket sort approach to solve Contains Duplicate 3 problem on Leetcode.

--- ContainsDuplicate3.java
+++ ContainsDuplicate3.java
@@ -1,7 +1,85 @@
+import java.util.HashMap;
+import java.util.Map;
 import java.util.SortedSet;
 import java.util.TreeSet;
 
 public class ContainsDuplicate3 {
+
+    /*
+     Time Complexity: O(n)
+     Space Complexity: O(min(n, k))
+
+     Creating buckets with width equal to desired difference in value.
+
+     No bucket must have more than one element at a time.
+     Each element could be paired with the element in its preceding and following buckets.
+     */
+    private int getBucketID(int num, int width) {
+
+        int bucketID;
+
+        width++;
+
+        if(num >= 0) {
+            bucketID = num / width;
+        }
+        else {
+            num = num * -1;
+
+            bucketID = num / width;
+
+            if(num % width != 0) {
+                bucketID++;
+            }
+
+            bucketID *= -1;
+        }
+
+        return bucketID;
+    }
+
+    public boolean containsNearbyAlmostDuplicate(int[] nums, int indexDiff, int valueDiff) {
+        boolean pairFound = false;
+        Map<Integer, Integer> bucketIDAndElement = new HashMap<Integer, Integer>();
+
+        for(int index = 0 ; index < nums.length ; index++) {
+            int currentNum = nums[index];
+
+            int bucketIDOfCurrentNum = getBucketID(currentNum, valueDiff);
+
+            if(bucketIDAndElement.containsKey(bucketIDOfCurrentNum)) {
+                pairFound = true;
+                break;
+            }
+            else {
+                if(bucketIDAndElement.containsKey(bucketIDOfCurrentNum - 1)) {
+                    int largestSmallerElement = bucketIDAndElement.get(bucketIDOfCurrentNum - 1);
+
+                    if(currentNum - largestSmallerElement <= valueDiff) {
+                        pairFound = true;
+                        break;
+                    }
+                }
+
+                if(bucketIDAndElement.containsKey(bucketIDOfCurrentNum + 1)) {
+                    int smallestLargerElement = bucketIDAndElement.get(bucketIDOfCurrentNum + 1);
+
+                    if(smallestLargerElement - currentNum <= valueDiff) {
+                        pairFound = true;
+                        break;
+                    }
+                }
+            }
+
+            bucketIDAndElement.put(bucketIDOfCurrentNum, currentNum);
+
+            if(index >= indexDiff) {
+                bucketIDAndElement.remove(getBucketID(nums[index - indexDiff], valueDiff));
+            }
+        }
+
+        return pairFound;
+    }
 
     /*
     Time : O(nLog(n)), for each number in nums we're checking whether any number in the range of permissible values
