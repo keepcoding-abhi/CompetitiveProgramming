73807a5be55884e3790b676102f0f23432289cf3
keepcoding-abhi <122338277+keepcoding-abhi@users.noreply.github.com>
2024-02-29 16:24:18 -0700

    Maximum sub array problem on Leetcode.

--- MaximumSubarray.java
+++ MaximumSubarray.java
@@ -23,6 +23,65 @@ public class MaximumSubarray {
     }
 }
 
+/*
+Time: O(n * Log(n))
+Space: O(Log(n))
+
+Divide and Conquer Solution. Divide the array from middle and recursively find the maximum sum in each half.
+To find the sum of the sub-array including the middle element, find maxSum starting from the last element in left half
+and the first element in right half assuming that all elements are part of the maxSum.
+ */
+class Solution {
+    public int maxSubArray(int[] nums) {
+        return maxSubArrayDAndC(nums, 0, nums.length - 1);
+    }
+
+    private int maxSubArrayDAndC(int[] nums, int left, int right) {
+
+        int result = Integer.MIN_VALUE;
+
+        if(left <= right) {
+
+            int mid = (left + right) / 2;
+            int maxSumLeft = maxSubArrayDAndC(nums, left, mid - 1);
+            int maxSumRight = maxSubArrayDAndC(nums, mid + 1, right);
+
+            int maxSumWithMid = nums[mid] + Math.max(findMaxSumRight(nums, mid + 1), 0) + Math.max(findMaxSumLeft(nums, mid - 1), 0);
+
+            result = Math.max(maxSumLeft, maxSumRight);
+            result = Math.max(result, maxSumWithMid);
+        }
+
+        return result;
+    }
+
+    private int findMaxSumRight(int[] nums, int start) {
+        int index = start, maxSum = Integer.MIN_VALUE, currentSum = 0;
+
+        while(index < nums.length) {
+            currentSum += nums[index];
+            maxSum = Math.max(currentSum, maxSum);
+            index++;
+        }
+
+        return maxSum;
+    }
+
+    private int findMaxSumLeft(int[] nums, int start) {
+
+        int maxSum = Integer.MIN_VALUE, currentSum = 0, index = start;
+
+        while(index > -1) {
+            currentSum += nums[index];
+            maxSum = Math.max(maxSum, currentSum);
+
+            index--;
+        }
+
+        return maxSum;
+    }
+}
+
 //1) O(n^2) Brute force solution. Find the sum of largest sub-array which starts at all positions of the array.
 
 class Solution {
